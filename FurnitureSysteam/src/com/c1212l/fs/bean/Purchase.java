f04b9a553a637973329ed5df56001373ee2f0094
amitkimdong <amitkimdong484@gmail.com>
2013-10-09 00:10:26 +0700

    create PurchaseDetail

--- FurnitureSysteam/src/com/c1212l/fs/bean/Purchase.java
+++ FurnitureSysteam/src/com/c1212l/fs/bean/Purchase.java
@@ -9,5 +9,71 @@ package com.c1212l.fs.bean;
  * @author Win 8 64Bit VS7
  */
 public class Purchase {
+    private String purID;
+    private String purDate;
+    private int purTotalPrice;
+    private int VAT;
+    private String Status;
+    private String empID;
+
+    public Purchase(String purID, String purDate, int purTotalPrice, int VAT, String Status, String empID) {
+        this.purID = purID;
+        this.purDate = purDate;
+        this.purTotalPrice = purTotalPrice;
+        this.VAT = VAT;
+        this.Status = Status;
+        this.empID = empID;
+        
+    }
+
+    public String getPurID() {
+        return purID;
+    }
+
+    public String getPurDate() {
+        return purDate;
+    }
+
+    public int getPurTotalPrice() {
+        return purTotalPrice;
+    }
+
+    public int getVAT() {
+        return VAT;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public String getEmpID() {
+        return empID;
+    }
+
+    public void setPurID(String purID) {
+        this.purID = purID;
+    }
+
+    public void setPurDate(String purDate) {
+        this.purDate = purDate;
+    }
+
+    public void setPurTotalPrice(int purTotalPrice) {
+        this.purTotalPrice = purTotalPrice;
+    }
+
+    public void setVAT(int VAT) {
+        this.VAT = VAT;
+    }
+
+    public void setStatus(String Status) {
+        this.Status = Status;
+    }
+
+    public void setEmpID(String empID) {
+        this.empID = empID;
+    }
+    
+    
     
 }
--- FurnitureSysteam/src/com/c1212l/fs/bean/PurchaseDetail.java
+++ FurnitureSysteam/src/com/c1212l/fs/bean/PurchaseDetail.java
@@ -9,5 +9,39 @@ package com.c1212l.fs.bean;
  * @author KimDong
  */
 public class PurchaseDetail {
+    private String purID;
+    private int purQuantity;
+    private int purPrice;
+
+    public PurchaseDetail(String purID, int purQuantity, int purPrice) {
+        this.purID = purID;
+        this.purQuantity = purQuantity;
+        this.purPrice = purPrice;
+    }
+
+    public String getPurID() {
+        return purID;
+    }
+
+    public int getPurQuantity() {
+        return purQuantity;
+    }
+
+    public int getPurPrice() {
+        return purPrice;
+    }
+
+    public void setPurID(String purID) {
+        this.purID = purID;
+    }
+
+    public void setPurQuantity(int purQuantity) {
+        this.purQuantity = purQuantity;
+    }
+
+    public void setPurPrice(int purPrice) {
+        this.purPrice = purPrice;
+    }
+    
     
 }
